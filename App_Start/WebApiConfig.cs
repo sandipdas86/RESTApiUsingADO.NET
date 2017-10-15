8263407cf52073144a967ad1d49087387fe68a91
sandipdas86 <sdas87@gmail.com>
2017-10-15 18:03:10 +0530

    Add files via upload

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+using System.Net.Http.Headers;
+
+namespace WebApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi1",
+                routeTemplate: "api/{controller}/{actionname}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi2",
+                routeTemplate: "api/{controller}/{actionname}",
+                defaults: new { actionname = RouteParameter.Optional }
+            );
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+        }
+    }
+}
---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.SqlClient;
+using System.Configuration;
+
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        [HttpGet]
+        [ActionName("GetEmployeeById")]
+        public HttpResponseMessage Get(int id)
+        {                
+                if (string.IsNullOrEmpty(id.ToString()))
+                {
+                    return Request.CreateResponse(HttpStatusCode.NoContent);
+                }
+                else
+                {
+                    SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmpDB"].ConnectionString.ToString());
+                    SqlCommand cmd = new SqlCommand("select * from TblRegister where Id=" + id, con);
+                    con.Open();
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    Employee emp = null;
+                    while (dr.Read())
+                    {
+                        emp = new Employee();
+                        emp.Id = Convert.ToInt32(dr.GetValue(0));
+                        emp.Name = dr.GetValue(1).ToString();
+                    }
+                    if (emp != null)
+                    {
+                        return Request.CreateResponse(HttpStatusCode.OK, emp);
+                    }
+                    else
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee not found");
+                    }
+                }
+        }
+    }    
+}
